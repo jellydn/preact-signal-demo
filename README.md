b0dc3ac0479d4780cb25f3b3d6eb28aa1f0c97e4
Huynh Duc Dung <dunghd.it@gmail.com>
2022-10-02 13:14:00 +0800

    init react app demo with Signal

---
+++ README.md
@@ -0,0 +1,40 @@
+# Welcome to signal-itman-demo 👋
+
+![Version](https://img.shields.io/badge/version-0.0.1-blue.svg?cacheSeconds=2592000)
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](#)
+[![Twitter: jellydn](https://img.shields.io/twitter/follow/jellydn.svg?style=social)](https://twitter.com/jellydn)
+
+> Manage state with style in every framework
+
+## Install
+
+```sh
+yarn install
+```
+
+## Usage
+
+```sh
+yarn dev
+```
+
+## Useful references
+
+- Introducing Signals https://preactjs.com/blog/introducing-signals/
+- Signal Boosting https://preactjs.com/blog/signal-boosting
+
+## Author
+
+👤 **Dung Huynh Duc**
+
+- Website: https://productsway.com/
+- Twitter: [@jellydn](https://twitter.com/jellydn)
+- Github: [@jellydn](https://github.com/jellydn)
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
+
+---
+
+_This README was generated with ❤️ by [readme-md-generator](https://github.com/kefranabg/readme-md-generator)_
---
+++ src/App.tsx
@@ -0,0 +1,56 @@
+import { computed, effect, signal, useComputed } from "@preact/signals-react";
+
+import "./App.css";
+import logo from "./logo.svg";
+
+const count = signal(0);
+const triple = computed(() => count.value * 3);
+
+effect(() => {
+  console.log("triple is now", triple.value);
+});
+
+function App() {
+  const double = useComputed(() => count.value * 2);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello Vite + React!</p>
+        <p>
+          <button onClick={() => count.value++}>
+            count is: {count.value}
+          </button>
+        </p>
+        <p>
+          Double: {double.value}
+        </p>
+        <p>
+          Triple: {triple.value}
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {" | "}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
